a59125ecc20cbe1087ff73e07bfd03feea5d6859
PaskoBerisic <paki.berka@gmail.com>
2022-06-09 19:28:01 +0200

    Application commit

---
+++ ApplicationCore/Interfaces/ICharacterCollectionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Entities;
+namespace ApplicationCore.Interfaces
+{
+    public interface ICharacterCollectionService
+    {
+        Task<IEnumerable<Hero>> GetAllHeroes();
+        Task<Hero> GetHeroById(int id);
+        Task<Hero> GetHeroByName(string name);
+        Task<Hero> AddHero(Hero hero);
+        Task UpdateHero(Hero hero);
+        Task DeleteHero(int id);
+    }
+}
---
+++ Infrastructure/Data/CharacterCollectionContext.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using Infrastructure.Extensions;
+
+namespace Infrastructure.Data
+{
+    public class CharacterCollectionContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+        public CharacterCollectionContext(DbContextOptions<CharacterCollectionContext> options, IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Villain> Villian { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Seed();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("CharacterCollectionDatabase"));
+        }
+    }
+}
